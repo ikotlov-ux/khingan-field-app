5c6a6a622900a18341c6d04111ff4e95d6f16ba9
ikotlov-ux <322125414+ikotlov-ux@users.noreply.github.com>
2026-09-24 14:29:35 +0000

    Add laboratory logo (lab-logo.png); SW v8

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Offline app-shell cache for Habitat 32. Bump CACHE on every release. */
-const CACHE = 'fp-shell-v7';
+const CACHE = 'fp-shell-v8';
 const SHELL = [
-  './', './index.html', './app.js', './classes.js', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png', './icon-180.png',
+  './', './index.html', './app.js', './classes.js', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png', './icon-180.png', './lab-logo.png',
   './vendor/ol.js', './vendor/ol.css', './vendor/jszip.min.js'
 ];
 self.addEventListener('install', (e) => {
